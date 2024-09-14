99f3951f5ee6af9677b00adbbe67f00cfc8365a8
David Brocker <davidabrocker@gmail.com>
2024-09-14 17:43:37 -0400

    Update global.R

--- global.R
+++ global.R
@@ -1,214 +1,323 @@
-# -------------------------------------------------------------------------
-# Load packages
-# -------------------------------------------------------------------------
-library(htmlwidgets)
-library(dplyr)
-library(tidyr)
-library(purrr)
-library(spotifyr)
+# Load Packages -------------------------------------------------------------------------------
 library(shiny)
-library(shinyjs)
-library(stringr)
-library(shinydashboard)
-library(ggraph)
-library(igraph)
-library(progressr)
-library(shinyalert)
-library(shinybrowser)
-library(shinydashboardPlus)
-library(shinycssloaders)
-library(shinyscreenshot)
-library(shinyvalidate)
-library(fontawesome)
+library(bslib)
+library(bsicons)
+library(spotifyr)
 library(visNetwork)
-# -------------------------------------------------------------------------
-# Set Environment
-# -------------------------------------------------------------------------
-Sys.setenv(SPOTIFY_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxxx')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxx')
+library(shinydashboard)
+library(purrr)
+library(dplyr)
+library(slickR)
+library(stringr)
+library(shinyWidgets)
+library(shinyalert)
+
+# API Stuff -----------------------------------------------------------------------------------
+Sys.setenv(SPOTIFY_CLIENT_ID = 'Shh It is a secret!')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'Shh It is a secret!')
 access_token <- get_spotify_access_token()
+# First Degree Net ----------------------------------------------------------------------------
+net <- function(artist){
+  
+  xx <- 
+    search_spotify(artist, type = "artist") |> 
+    filter(name == artist) |> 
+    filter(popularity == max(popularity)) |> 
+    mutate(image = map(images, ~.x[1,2]) |> unlist())
 
-# -------------------------------------------------------------------------
-# For Example -------------------------------------------------------------
-# -------------------------------------------------------------------------
-# Aesop Rock and Dessiderium: No Matches
-ex1_nodes <- read.csv("https://raw.githubusercontent.com/DavidBrocker/MusicMatchup/main/Examples/ar_d_node.csv")
-ex1_edge <- read.csv("https://raw.githubusercontent.com/DavidBrocker/MusicMatchup/main/Examples/ar_d_edge.csv")
-
-# Allie X and Magdalena Bay: Matches
-ex2_nodes <- read.csv("https://raw.githubusercontent.com/DavidBrocker/MusicMatchup/main/Examples/ax_mb_node.csv")
-ex2_edge <- read.csv("https://raw.githubusercontent.com/DavidBrocker/MusicMatchup/main/Examples/ax_mb_edge.csv")
-# -------------------------------------------------------------------------
-# Establish Functions -----------------------------------------------------
-# -------------------------------------------------------------------------
-artist_similarity2 <- function(artist){
-  # Get similar artists
-  artist_sim <-
-    # Need to use GAF to get artist_id
-    get_artist_audio_features(artist,
-                              include_groups = c("album", "single")) |>
-    # Isolate id
-    select(artist_id) |>
-    # Get Unique value
-    unique() |>
-    # Pipe into get_related_artists
+  xxx <- 
+    xx |> 
+    select(id) |> 
     get_related_artists() |> 
-    # Unnest by the id column
-    unnest(id, names_sep = "") |>
-    # Make the id column contain the related artists for that id
-    mutate(id = id |> map(get_related_artists)) |>
-    # Get full list
-    unnest(id, names_sep = "id") |> 
-    mutate(original_artist = artist) |> 
-    rename(parent_artist = name,
-           related_artist = ididname,
-           related_artist_genres = ididgenres,
-           related_artist_followers.total = ididfollowers.total,
-           related_artist_images = ididimages) |> 
-    select(original_artist,parent_artist,genres,followers.total,
-           related_artist, images,related_artist, related_artist_genres,
-           related_artist_followers.total, related_artist_images)
+    mutate(from = artist,
+           image = map(images, ~.x[1,1]) |> unlist()) |> 
+    select(-followers.href, -type, -images,-href) 
+  
+  xxx <- 
+    xxx |> 
+    bind_rows(xx)
+  
+  nodes <- 
+    tibble(
+      id = xxx$name,
+      value = xxx$followers.total,
+      image = xxx$image,
+      shape = "circularImage"
+    )
+  
+  edges <- 
+    tibble(
+      from = xxx$from,
+      to = xxx$name
+    )
+  
+  visNetwork(nodes,edges, width = "100%") |> 
+    visNodes(
+      shadow = TRUE,
+      color = 
+        list(
+          background = "#f5f6f7",
+          border = "#F5F6F7",
+          highlight = 
+            list(
+              background = "#f5f6f7",
+              border = "#f5f6f7"
+              )
+          )
+      ) |> 
+    visEdges(
+      color = 
+        list(
+          highlight = "#f5f6f7"),
+             width = .5) |> 
+    visOptions(
+      nodesIdSelection = 
+        list(enabled = TRUE,
+             main = "Select Artist...")) 
+
 }
 
-# ---------------------------------------------------------------------------------------------
-# Artist Similarity First Order Only ----------------------------------------------------------
-# ---------------------------------------------------------------------------------------------
-artist_similarity <- function(artist){
-  # Get similar artists
-  artist_sim <-
-    # Need to use GAF to get artist_id
-    get_artist_audio_features(artist,
-                              include_groups = c("album", "single")) |>
-    # Isolate id
-    select(artist_id) |>
-    # Get Unique value
-    unique() |>
-    # Pipe into get_related_artists
-    get_related_artists() |> 
-    mutate(original_artist = artist) |> 
-    rename(parent_artist = name) |> 
-    select(original_artist,parent_artist,genres,followers.total,
-           images)
-}
-
-# ------------------------------------------------------------------------------
-# Test for Similarity: First Order ---------------------------------------------
-# ------------------------------------------------------------------------------
-first_order <- function(artist) {
-  artist <-
-    artist |>
-    select(original_artist, parent_artist) |>
-    group_by(original_artist) |>
-    unique() |>
-    group_by(parent_artist) |>
-    summarise(isDup = n() > 1,
-              n = n()) |>
-    group_by(isDup) |>
-    count() -> er_mgmt
-  er_mgmt
-  if (length(er_mgmt[er_mgmt$isDup == TRUE]) < 1) {
-    "0%" -> pct_alike
-  } else {
-    er_mgmt |>
-      ungroup() |>
-      mutate(alike = n / sum(n) * 100) |>
-      filter(isDup == "TRUE") |>
-      select(alike) |>
-      as.numeric() |>
-      round(2) |>
-      paste0("%") -> pct_alike
+# Interconnection Function -------------------------------------------------------------------------
+inter_fun <- function(artist,level){
+  if(level == 1) {
+    xx <- 
+      search_spotify(artist, type = "artist") |> 
+      filter(name == artist) |> 
+      filter(popularity == max(popularity)) |> 
+      mutate(image = map(images, ~.x[1,2]) |> unlist())
+    
+    xxx <- 
+      xx |> 
+      select(id) |> 
+      get_related_artists() |> 
+      mutate(from = artist,
+             image = map(images, ~.x[1,2]) |> unlist()) |> 
+      select(-followers.href, -type, -images,-href) |> 
+      bind_rows(xx)
+    
+    nodes <- 
+      tibble(
+        id = xxx$name,
+        value = xxx$followers.total,
+        image = xxx$image,
+        shape = "circularImage"
+      )
+    
+    edges <- 
+      tibble(
+        from = xxx$from,
+        to = xxx$name
+      )
+    
+    visNetwork(nodes,edges) 
+    
+  } else{
+    fd <- 
+      search_spotify(artist, type = "artist") |>
+      filter(name == artist) |> 
+      filter(popularity == max(popularity)) |> 
+      select(id) |> 
+      get_related_artists() |> 
+      select(-followers.href,-type) |> 
+      relocate(name, .before = genres) |> 
+      mutate(from = artist) |> 
+      mutate(image = map(images, ~.x[1,2]) |> unlist())
+    
+    sed <- 
+      map_df(fd$id, get_related_artists) |> 
+      select(-followers.href,-type) |> 
+      relocate(name, .before = genres) |> 
+      mutate(image = map(images, ~.x[1,2]) |> unlist())
+    
+    
+    
+    full <- 
+      fd |> 
+      bind_rows(sed) |> 
+      mutate(from = c(fd$from,rep(fd$name,each = 20)))
+    
+    # Inter-Connection
+    in_full <- 
+      full |> 
+      select(name,from,image,followers.total) |> 
+      filter(name %in% from) |> 
+      group_by(name) |> 
+      unique()
+    
+    nodes_in <- 
+      tibble(
+        id = unique(in_full$name),
+        image = unique(in_full$image),
+        shape = "circularImage",
+        value = unique(in_full$followers.total)
+      )
+    
+    edges_in <- 
+      tibble(
+        from = in_full$from,
+        to = in_full$name
+      )
+    
+    visNetwork(nodes_in,edges_in)  |> 
+      visNodes(shadow = TRUE,
+               color = 
+                 list(
+                   background = "#f5f6f7",
+                   border = "#F5F6F7",
+                   highlight = 
+                     list(
+                       background = "#f5f6f7",
+                       border = "#f5f6f7"
+                     )
+                 )
+      ) |>
+      visEdges(color = "black",
+               width = .5) |>
+      visOptions(
+        highlightNearest =
+          list(enabled = TRUE,
+               labelOnly = FALSE,
+               hideColor = "rgba(255,255,255,0)"),
+        nodesIdSelection =
+          list(enabled = TRUE,
+               main = "Select Artist...")) |>
+      visPhysics(enabled = FALSE) |> 
+      visIgraphLayout(layout = "layout_in_circle")
   }
-  mget(ls())
 }
-# ------------------------------------------------------------------------------
-# Test for Similarity: Second Order --------------------------------------------
-# ------------------------------------------------------------------------------
-second_order <- function(artist) {
-  artist |>
-    select(original_artist, related_artist) |>
-    group_by(related_artist) |>
-    unique() |>
-    summarize(isDup = n() > 1, n = n()) |>
-    group_by(isDup) |>
-    count() -> er_mgmt
-  if (length(er_mgmt[er_mgmt$isDup == TRUE]) < 1) {
-    "0%" -> pct_alike
-  } else {
-    er_mgmt |>
-      ungroup() |>
-      mutate(alike = n / sum(n) * 100) |>
-      filter(isDup == "TRUE") |>
-      select(alike) |>
-      as.numeric() |>
-      round(2) |>
-      paste0("%") -> pct_alike
-  }
-  mget(ls())
+# Interactive Search --------------------------------------------------------------------------
+artsrch <- function(artist){
+  search_spotify(artist,"artist") |> 
+    filter(!duplicated(name))
 }
+# Define Cards --------------------------------------------------------------------------------
+# Visnetwork
+card_vis <- 
+  card(
+    class = "net_card",
+    card_header(
+      "Related Artist Network",
+      tooltip(
+        bs_icon("diagram-3"),
+        "20 Related Artists based on User Listening History",
+        placement = "bottom")),
+    card_body(
+      visNetworkOutput("net"),
+    )
+  )
 
-# ---------------------------------------------------------------------------------------------
-# Create Function: Get Main Artist Images -----------------------------------------------------
-# ---------------------------------------------------------------------------------------------
-two_pics <- function(x){
-  print('begin function...')
-  get_artist_audio_features(x,
-                            include_groups = c("album","single")) |> 
-    select(artist_id) |> 
-    unique() |> 
-    get_artists() |> 
-    mutate(image = map(images,~.x[1,2]) |> unlist()) |>
-    select(name,image) |> 
-    rename(id = name)
-}
-# -------------------------------------------------------------------------
-# Create Function: Artist Network Graphing --------------------------------
-# -------------------------------------------------------------------------
-artist_vis <- function(artist, tp) {
-  # Create Node Dataframe from tp function
-  node <- tp
-  
-  # Reformat with artist Dataframe
-  node_2 <-
-    artist |>
-    select(parent_artist, images) |>
-    mutate(image = map(images, ~ .x[1, 2]) |> unlist()) |>
-    distinct(parent_artist, .keep_all = T) |>
-    rename(id = parent_artist) |>
-    select(-images)
-  
-  # Combine
-  node <-
-    node |>
-    full_join(node_2)
-  
-  # Create Node Label
-  node$label <- node$id
-  
-  print(node)
-  
-  # Create Node Shape
-  node$shape <- "circularImage"
-  
-  node$color <- "white"
-  
-  # Create Edge Label
-  edge <-
-    artist |>
-    select(original_artist, parent_artist) |>
-    unique() |>
-    rename(from = original_artist,
-           to = parent_artist)
-  
-  visNetwork(node, edge) |>
-    visNodes(shapeProperties = list(useBorderWithImage = TRUE),
-             font = list(color = "white")) |> 
-    visOptions(nodesIdSelection = T) |> 
-    visEvents(type = "once", startStabilizing = "function() {
-            this.moveTo({scale:0.3})}")
-}
+# Card 2: Searchbar
+card_srch <- 
+  card(
+    class = "main_card",
+    card_header(
+      "MusicMatchup",
+      popover(
+        actionLink("link", bs_icon("music-note")),
+        "Data provided from ",
+        a("Spotify API", href = "https://developer.spotify.com/documentation/web-api"))
+    ),
+    card_body(
+      class = "main_card_body",
+        textInput(
+          inputId = "artist",
+          label =  NULL,
+          placeholder = "Enter Artist",
+          value = "",
+          width = "25%"
+        ),
+          pickerInput(
+            inputId = "art_dropdown",
+            label = "",
+            choices = "",
+            inline = FALSE,
+            width = "fit",
+            options = 
+              pickerOptions(
+                noneResultsText = "No Matching Artists Found",
+                noneSelectedText = "No Artist Searched"
+              )
+            ),
+        actionBttn(
+          inputId = "search",
+          label = "Search",
+          icon = icon("search"),
+          size = "sm"
+        ),
+      )
+  )
 
+# Card 3 - Artist Name
+card_name <- 
+  card(
+    class = "name_card",
+    card_header("Artist Name"),
+    card_body(
+      uiOutput("art_name")
+    ),
+    card_footer(
+      div(class = "rfsh",
+        actionBttn(
+          inputId = "search2",
+          label = "Re-Roll",
+          icon = icon("refresh"),
+          size = "sm",
+          color = "royal"
+        ),
+        tooltip(
+          bs_icon("info-circle"),
+          "Re-search with selected artist",
+          placement = "right")
+      )
+    )
+  )
+# Card 4 - Artist Followers 
+card_fol <- 
+  card(
+    class = "fol_card",
+    card_header("Followers"),
+    card_body(
+      uiOutput("art_pop")
+    )
+  )
 
-# Validation Function -------------------------------------------------------------------------
-art_check <- function(x){
-  str_detect(x,"\\w+,\\s\\w+")
-}
+# Card 5 - Track Preview
+card_pre <- 
+  card(
+    class = "pre_card",
+    card_header("Top Track Preview"),
+    card_body(
+      uiOutput("art_pre")
+    )
+    # card_footer(
+    #   splitLayout(
+    #     cellWidths = c("75%","25%"),
+    #     bs_icon(
+    #       "person-fill-add",
+    #       size = "2em",
+    #       margin_left = "5%"),
+    #     bs_icon(
+    #       "plus-circle-fill",
+    #       size = "2em",
+    #       class = "text-success",
+    #       margin_left = "10%")
+    #     )
+    #   )
+  )
+
+# Card 6 - Artist Genres
+card_gen <- 
+  card(
+    class = "gen_card",
+    card_header("Genres"),
+    card_body(
+      uiOutput("art_gen")
+    )
+    # card_footer(
+    #   bs_icon(
+    #     "plus-circle-fill",
+    #     size = "2em",
+    #     class = "text-suc <- cess",
+    #     width = "100%")
+    # )
+  )
